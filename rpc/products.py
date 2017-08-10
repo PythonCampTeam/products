bdec5bfbbeee01c5c2bbff05a53e5c7083bba8f3
Varvara <varvara.malysheva@saritasa.com>
2017-08-10 18:31:41 +0700

    created GET /api/products/ID

--- rpc/products.py
+++ rpc/products.py
@@ -1,4 +1,5 @@
 from nameko.rpc import rpc
+import stripe
 
 
 class Products(object):
@@ -11,8 +12,16 @@ class Products(object):
         """
     name = 'ProductsRPC'
 
+    stripe.api_key = "sk_test_K5QUkUgvUNKvDD9fEGYBI6Gi"
+
     @rpc
-    def getproduct(self, **kwargs):
+    def testing(self, **kwargs):
         doc_class = self.__dict__
         return {self.__class__.__name__: doc_class,
                 'docs': self.__class__.__doc__}
+
+    @rpc
+    def getproduct(self, ID):
+        stripe.api_key = "sk_test_K5QUkUgvUNKvDD9fEGYBI6Gi"
+        item = stripe.Product.retrieve(ID)
+        return item
