70db741e54b93a6a7a33aed7eaafbc4490f46608
Varvara <varvara.malysheva@saritasa.com>
2017-08-15 13:01:26 +0700

    Created searched methods

--- rpc/products.py
+++ rpc/products.py
@@ -63,7 +63,8 @@ class Products(object):
         Args:
             count(int) A limit on the number of objects to be returned.
                        The default is 100 items.
-            Returns a list of products objects if the call succeeded"""
+            item (list) Returns a list of products objects if the call succeeded
+            """
         item = stripe.Product.list(limit=count)
         return item
 
@@ -73,12 +74,12 @@ class Products(object):
         Args:
             ID (string) The ID of the product to delete.
         Returns:
-            Returns an object with a deleted parameter on success.
+            result (object) Returns an object with a deleted parameter on success.
             Otherwise, this call raises an error.
         """
         product = stripe.Product.retrieve(ID)
-        res = product.delete()
-        return res
+        result = product.delete()
+        return result
 
     @rpc
     def update_product(self, id_product, key, value):
@@ -90,18 +91,39 @@ class Products(object):
             key (string) The parameter to update.
             value New value for the parameter
         Returns:
-            Returns the product object if the update succeeded.
+            result (object) Returns the product object if the update succeeded.
         """
         product = stripe.Product.retrieve(id_product)
         product[key] = value
-        res = product.save()
-        return res
+        result = product.save()
+        return result
 
     @rpc
     def filter_products(self, category):
-        """Returns the filtering product list"""
+        """Returns the filtering product list
+        Args:
+            category (stripe) parameter for search
+            items (list) List of all products
+        Returns
+            result (list) sorted list products"""
         items = stripe.Product.list(limit=100)["data"]
-        result = [it for it in items if it.metadata.get("category") == category]
+        result = [it for it in items if it.metadata.get("category")
+                  == category]
+        return result
+
+    @rpc
+    def search_products(self, search):
+        """Returns a list of products with a given parameter
+        Args:
+            search (stripe) parameter for search
+            items (list) List of all products
+        Returns
+            result (list) sorted list products"""
+        items = stripe.Product.list(limit=100)["data"]
+        result = [it for it in items if it.get("name") == search or
+                  it.metadata.get("type") == search or
+                  it.metadata.get("category") == search or
+                  it.metadata.get("for") == search]
         return result
 
     @rpc
@@ -112,34 +134,24 @@ class Products(object):
             items (list) List of objects products
             prod_sorted (list) sorted products list
         Returns:
-            sorted products list"""
+            prod_sorted (list) sorted products list"""
         items = stripe.Product.list(limit=100)["data"]
-        list_sorted = [it.get(sorty_value) for it in items]
-        list_sorted.sort(reverse=DESC)
         prod_sorted = []
+        list_sorted = [it.get(sorty_value) for it in items if
+                       it.get(sorty_value) is not None]
+        if list_sorted == []:
+            list_sorted = [it.metadata.get(sorty_value) for it in items if
+                           it.metadata.get(sorty_value) is not None]
+            list_sorted = list(set(list_sorted))
+            list_sorted.sort(reverse=DESC)
+            for it in list_sorted:
+                for item in items:
+                    if it == item.metadata.get(sorty_value):
+                        prod_sorted.append(item)
+            return prod_sorted
+        list_sorted.sort(reverse=DESC)
         for it in list_sorted:
             for item in items:
                 if it == item.get(sorty_value):
                     prod_sorted.append(item)
         return prod_sorted
-
-    # @rpc
-    # def sorted_products(sorty_value):
-    #     items = stripe.Product.list(limit=100)["data"]
-    #     prod_sorted = []
-    #     list_sorted = [it.get(sorty_value) for it in items if
-    #                    it.get(sorty_value) is not None]
-    #     if list_sorted == []:
-    #         list_sorted = [it.metadata.get(sorty_value) for it in items]
-    #         list_sorted = list(set(list_sorted))
-    #         list_sorted.sort()
-    #         for it in list_sorted:
-    #             for item in items:
-    #                 if it == item.metadata.get(sorty_value):
-    #                     prod_sorted.append(item)
-    #     list_sorted.sort()
-    #     for it in list_sorted:
-    #         for item in items:
-    #             if it == item.get(sorty_value):
-    #                 prod_sorted.append(item)
-    #     return prod_sorted
