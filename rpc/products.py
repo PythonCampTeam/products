7a1177247e8f64f2a1b1c6d376907496066e37df
Varvara <varvara.malysheva@saritasa.com>
2017-08-15 19:11:20 +0700

    Fix methods sort, filter, order

--- rpc/products.py
+++ rpc/products.py
@@ -1,5 +1,6 @@
 from nameko.rpc import rpc
 import stripe
+import operator
 
 
 class Products(object):
@@ -28,9 +29,8 @@ class Products(object):
         return item
 
     @rpc
-    def create_product(self, name, description,
-                       attributes, package_dimensions, metadata):
-        """Creted a new product
+    def create_product(self, convert):
+        """Creted a new product whith SKU object
         Args:
             name (string) The product’s name, for the customer.
             description (string) The product’s description for the customer.
@@ -45,21 +45,39 @@ class Products(object):
                 width (decimal) Width, in inches.
             metadata (hash) Set of key/value pairs that you can attach to
                             an object. Use as category
+            inventory (hash) Description of the SKU’s inventory.
         Returns:
             Returns a product object if the call succeeded.
             """
+        name = convert.get("name")
+        description = convert.get("description")
+        attributes = convert.get("attributes")
+        package_dimensions = convert.get("package_dimensions")
+        metadata = convert.get("metadata")
+        attributes_sku = convert.get("attributes_sku")
+        price = convert.get("price")
+        inventory = convert.get("inventory")
         item = stripe.Product.create(
-         name=name,
-         description=description,
-         attributes=attributes,
-         package_dimensions=package_dimensions,
-         metadata=metadata
+            name=name,
+            description=description,
+            attributes=attributes,
+            package_dimensions=package_dimensions,
+            metadata=metadata
          )
-        return item
+        stripe.SKU.create(
+            product=item.id,
+            attributes=attributes_sku,
+            price=price,
+            package_dimensions=package_dimensions,
+            currency="usd",
+            inventory=inventory
+        )
+
+        return stripe.Product.retrieve(item.id)
 
     @rpc
     def list_products(self, count=100):
-        """Return list products (max 100 item)
+        """Return list products (max 100 items)
         Args:
             count(int) A limit on the number of objects to be returned.
                        The default is 100 items.
@@ -99,24 +117,33 @@ class Products(object):
         return result
 
     @rpc
-    def filter_products(self, category):
+    def filter_products(self, category, order_by, DESC):
         """Returns the filtering product list
         Args:
             category (stripe) parameter for search
             items (list) List of all products
+            order_by (str) parameter for to ordering
+            DESC (bool) sorting direction
+            result (list) list products sorting by category
         Returns
-            result (list) sorted list products"""
+            sort (list) sorted list products"""
         items = stripe.Product.list(limit=100)["data"]
         result = [it for it in items if it.metadata.get("category")
                   == category]
-        return result
+        if order_by == "None" or order_by == "null":
+            return result
+        sort = Products.sorted(result, order_by, DESC)
+        return sort
 
     @rpc
-    def search_products(self, search):
+    def search_products(self, search, order_by, DESC):
         """Returns a list of products with a given parameter
         Args:
-            search (stripe) parameter for search
+            search (string) parameter for search
             items (list) List of all products
+            order_by (string) parameter for to ordering
+            DESC (bool) sorting direction
+            result (list) list products sorting by search
         Returns
             result (list) sorted list products"""
         items = stripe.Product.list(limit=100)["data"]
@@ -124,34 +151,42 @@ class Products(object):
                   it.metadata.get("type") == search or
                   it.metadata.get("category") == search or
                   it.metadata.get("for") == search]
-        return result
+        if order_by == "None" or order_by == "null":
+            return result
+        sort = Products.sorted(result, order_by, DESC)
+        return sort
 
     @rpc
     def sorted_products(self, sorty_value, DESC):
+        """Returns the sorted ordered list
+        Args:
+            items (list) List of all products
+            sorty_value (string) parameter for to ordering
+            DESC (bool) sorting direction
+        Return:
+            sort (list) list products ordered"""
+        items = stripe.Product.list(limit=100)["data"]
+        sort = Products.sorted(items, sorty_value, DESC)
+        return sort
+
+    def sorted(items, sorty_value, DESC):
         """Returns the sorted product list
         Args:
-            sorty (str) parameter for to sorty
+            sorty_value (str) parameter for to sorting
             items (list) List of objects products
             prod_sorted (list) sorted products list
+            func_sort (func) Function for sorting
+            DESC (bool) sorting direction
         Returns:
-            prod_sorted (list) sorted products list"""
-        items = stripe.Product.list(limit=100)["data"]
-        prod_sorted = []
-        list_sorted = [it.get(sorty_value) for it in items if
-                       it.get(sorty_value) is not None]
-        if list_sorted == []:
-            list_sorted = [it.metadata.get(sorty_value) for it in items if
-                           it.metadata.get(sorty_value) is not None]
-            list_sorted = list(set(list_sorted))
-            list_sorted.sort(reverse=DESC)
-            for it in list_sorted:
-                for item in items:
-                    if it == item.metadata.get(sorty_value):
-                        prod_sorted.append(item)
-            return prod_sorted
-        list_sorted.sort(reverse=DESC)
-        for it in list_sorted:
-            for item in items:
-                if it == item.get(sorty_value):
-                    prod_sorted.append(item)
-        return prod_sorted
+            Sorted products list"""
+        def sorty_price(item):
+            """Function for sort by price"""
+            return item.skus.data[0].price
+        if sorty_value == "price":
+            func_sort = sorty_price
+            return sorted(items, key=func_sort, reverse=DESC)
+        if items[0].get(sorty_value) is None:
+            func_sort = operator.attrgetter('metadata.'+sorty_value)
+            return sorted(items, key=func_sort, reverse=DESC)
+        func_sort = operator.attrgetter(sorty_value)
+        return sorted(items, key=func_sort, reverse=DESC)
