238565aae1640b8653422bbb21d3162bc1a5c96d
Varvara <varvara.malysheva@saritasa.com>
2017-08-25 11:20:49 +0700

    refractoring kwargs

--- rpc/products.py
+++ rpc/products.py
@@ -1,5 +1,4 @@
 import operator
-
 import cerberus
 import stripe
 from nameko.rpc import rpc
@@ -7,30 +6,42 @@ from rpc import validate
 from config.settings.common import security
 
 Validator = cerberus.Validator
-
 schema = validate.schema_product
 
 
 def get_object(id_product):
-    """Retutn product object by id"""
+    """Private method retutn product object by id"""
     return stripe.Product.retrieve(id_product)
 
 
 class Products(object):
     """ This class created Products and retrieve list of products,
-    delete product, update product, add product
-    Note: See https://stripe.com"""
+        delete product, update product, add product.
+
+    Note: See https://stripe.com
+
+    """
     name = 'ProductsRPC'
 
     stripe.api_key = security.api_key
 
     @rpc
+    def get_sku_product(self, id_product):
+        product = get_object(id_product)
+        return product.skus.data[0].id
+
+    @rpc
     def get_product(self, id_product):
         """Return product on Id
+
         Args:
             id_product (syring) The identifier for the product
+
         Returns:
-            Returns a product object if the call succeeded."""
+            Returns a product object if the call succeeded.
+            Otherwise, this call raises an error.
+
+        """
         try:
             item = get_object(id_product)
         except stripe.error.InvalidRequestError as e:
@@ -43,10 +54,11 @@ class Products(object):
     @rpc
     def create_product(self, body):
         """Creted a new product whith SKU object
+
         Args:
             name (string) The product’s name, for the customer.
             description (string) The product’s description for the customer.
-            attributes (lost of strings) A list of up to 5 attributes that each
+            attributes (list of strings) A list of up to 5 attributes that each
                                          SKU can provide values for
             package_dimensions (hash) The dimensions of this product for
                                       shipping purposes:
@@ -58,12 +70,14 @@ class Products(object):
             metadata (hash) Set of key/value pairs that you can attach to
                             an object. Use as category
             inventory (hash) Description of the SKU’s inventory.
+
         Returns:
             Returns a product object if the call succeeded.
-            """
+
+        """
         v = Validator()
         if not v.validate(body, schema):
-            return False
+            raise {"errors": v.error}
         name = body.get("name")
         description = body.get("description")
         attributes = body.get("attributes")
@@ -92,15 +106,18 @@ class Products(object):
     @rpc
     def delete_product(self, id_product):
         """Delete a product.
+
         Args:
             id_product (string) The ID of the product to delete.
             sku_id (string) The ID of the SKU of product
             sku (object) object SKU to delete
             product (object) object Product to delete
+
         Returns:
             result (object) Returns an object with a deleted parameter on
             success.
             Otherwise, this call raises an error.
+
         """
         product = get_object(id_product)
         try:
@@ -120,12 +137,16 @@ class Products(object):
     def update_product(self, body):
         """Updates the specific product by setting the values of the parameters
            passed.
+
         Note: Note that a product’s attributes are not editable.
+
         Args:
             id_product (string) The ID of the product to update.
             body (dict) The parameter to update.
+
         Returns:
             result (object) Returns the product object if the update succeeded.
+
         """
         id_product = body.pop("id")
         product = stripe.Product.retrieve(id_product)
@@ -137,14 +158,18 @@ class Products(object):
     @rpc
     def search_products(self, search, order_by, desc):
         """Returns a list of products with a given parameter
+
         Args:
             search (string) parameter for search
             items (list) List of all products
             order_by (string) parameter for to ordering
             desc (bool) sorting direction
             result (list) list products sorting by search
+
         Returns
-            result (list) sorted list products"""
+            result (list) sorted list products
+
+        """
         items = stripe.Product.list(limit=100)["data"]
         if search is False:
             result = items
@@ -158,14 +183,18 @@ class Products(object):
 
     def sorted(items, sorty_value, desc):
         """Returns the sorted product list
+
         Args:
             sorty_value (str) parameter for to sorting
             items (list) List of objects products
             prod_sorted (list) sorted products list
             func_sort (func) Function for sorting
             desc (bool) sorting direction
+
         Returns:
-            Sorted products list"""
+            Sorted products list
+
+        """
         def sorty_price(item):
             """Function for sort by price"""
             return item.skus.data[0].price
--- rpc/validate.py
+++ rpc/validate.py
@@ -1,24 +1,52 @@
-schema_product = {'name': {'type': 'string'},
+schema_product = {'name': {'type': 'string', 'empty': False, 'required': True},
                   'attributes': {'type': 'list'},
                   'description': {'type': 'string'},
                   'package_dimensions':
-                  {'type': 'dict', 'schema':
-                                   {'height': {'type': 'float'},
-                                    'width': {'type': 'float'},
-                                    'weight': {'type': 'float'},
-                                    'length': {'type': 'float'}
-                                    }},
-                  'metadata': {'type': 'dict', 'schema':
-                                               {'category': {'type': 'string'},
-                                                'for':  {'type': 'string'},
+                  {'type': 'dict',  'empty': False, 'required': True,
+                                    'schema':
+                                    {'height':
+                                     {'type': 'float',
+                                      'empty': False,
+                                      'required': True},
+                                     'width':
+                                     {'type': 'float',
+                                      'empty': False,
+                                      'required': True},
+                                     'weight':
+                                     {'type': 'float',
+                                      'empty': False,
+                                      'required': True},
+                                     'length':
+                                     {'type': 'float',
+                                      'empty': False,
+                                      'required': True}
+                                     }},
+                  'metadata': {'type': 'dict', 'empty': False,
+                               'required': True, 'schema':
+                                               {'category':
+                                                {'type': 'string',
+                                                 'empty': False,
+                                                 'required': True},
+                                                'for':
+                                                {'type': 'string',
+                                                 'empty': False,
+                                                 'required': True},
                                                 'type': {'type': 'string'}
                                                 }},
-                  'attributes_sku': {'type': 'dict'},
-                  'price':  {'type': 'integer'},
-                  'inventory': {'type': 'dict', 'schema':
-                                                {'type': {'type': 'string'},
+                  'attributes_sku': {'type': 'dict',
+                                     'dependencies': 'attributes'},
+                  'price':  {'type': 'integer', 'empty': False,
+                             'required': True},
+                  'inventory': {'type': 'dict', 'empty': False,
+                                'required': True, 'schema':
+                                                {'type':
+                                                 {'type': 'string',
+                                                  'empty': False,
+                                                  'required': True},
                                                  'quantity':
-                                                 {'type': 'integer'}
+                                                 {'type': 'integer',
+                                                  'empty': False,
+                                                  'required': True}
                                                  }
                                 }
                   }
